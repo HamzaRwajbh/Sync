fb1b42fc765da544d385177982f526458a6dc78c
Hamza Alrwajbh <hamza.alrwajbh@outlook.com>
2018-02-18 13:38:23 +0200

    the task is completed without Junit testing

--- src/SyncFile/Synchronize.java
+++ src/SyncFile/Synchronize.java
@@ -1,7 +1,4 @@
 package SyncFile;
-
-
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,41 +11,51 @@ public class Synchronize {
 
     public Synchronize() {}
 
-    public void syncFolderEdit(File source , File target) throws IOException {
+    public void syncFolder(File source , File target) throws IOException {
 
-        File []src = source.listFiles();
-        Set<File> srcName = new HashSet<>(Arrays.asList(source));
+        System.out.println("We are in "+source.getAbsolutePath());
+        System.out.println("_____________________________________________________________________________________________");
+
         //delete all file not found in source
         for(File subFile : target.listFiles()){
-            System.out.println(subFile.getName());
-            if(!srcName.contains(subFile)){
+            if(!new File(source.getAbsolutePath(),subFile.getName()).exists()){
+                System.out.println(subFile.getName() + " doesn't exist in source");
                 delete(new File(target,subFile.getName()));
+                System.out.println(subFile.getAbsolutePath() + " is deleted");
             }
         }
         //check files exist in sources and not found in target & check files are updated in source
         for (File i : source.listFiles()) {
-            //if file exist in target
-            if (isExist(i.getName(), target.getAbsolutePath())) {
+            //if it is the directory
+            if (i.isDirectory()) {
                 //if the file is directory ? true : recall the syncFolder again until find file with extension
-                if (i.isDirectory()) {
+                if (isExist(i.getName(), target.getAbsolutePath())) {
                     System.out.println(i.getAbsolutePath() + " is a Directory and Exist");
-                    syncFolderEdit(i, new File(target.getAbsolutePath(),i.getName()));
+                    syncFolder(i, new File(target.getAbsolutePath(), i.getName()));
                 } else {
-                    // if the file with extension
+                    System.out.println(i.getAbsolutePath() + "is a Directory and not Exist");
+                    System.out.println(new File(target.getAbsolutePath(), i.getName()).mkdir() + " " + i.getAbsolutePath() + " is created");
+                    syncFolder(i, new File(target.getAbsolutePath(), i.getName()));
+                }// if the file with extension
+            }else{
+                if(isExist(i.getName(), target.getAbsolutePath())){
                     if (!isSync(i , new File(target,i.getName()))) { //check if the file is NOT sync ? true : copy to target
-                        //copy to target;
+                        System.out.println(i.getAbsolutePath() + " is not sync , the copy is done");
+                        copy(i,target);
                     } else { // if the file is NOT sync ? false : skip this file and continue
                         continue;
                     }
+                }else{
+                    copy(i,target);
+                    System.out.println(i.getName()+" is not found in target , copy is done");
                 }
-            } else {// if the file does NOT exist in target regardless if it was file or directory
-                //copy to target
+
             }
         }
+        System.out.println();
     }
 
     public boolean isExist(String name, String path){
-
         File temp = new File(path+File.separator+name);
         return temp.exists();
     }
@@ -56,51 +63,14 @@ public class Synchronize {
     public boolean isSync(File src ,File trg){
         long targetModifiedTime = trg.lastModified();
         long sourceModifiedTime = src.lastModified();
-        if(sourceModifiedTime < targetModifiedTime){
+        if(sourceModifiedTime <= targetModifiedTime){
             return true ;
         }
         return false ;
     }
 
-    /*this method take to parameter (the file show copy, and the file will copy to)
-    //
-    //will check if the source is a directory,
-    //  if (true)
-    //      the target should create new folder with the same name of source file
-    //      and recall copyDirectory(
-    //  else
-    //
-    */
     public void copy(File source, File target) throws IOException {
-
-        if(source.isDirectory() && target.isDirectory()){
-            System.out.println(new File(target.getAbsolutePath()+File.separator+source.getName()).mkdir());
-            copyContent(source,target);
-        }else if(source.isFile() && target.isDirectory()){
-            System.out.println(source.toPath());
-            System.out.println(target.toPath());
-            Files.copy(source.toPath(),target.toPath(),StandardCopyOption.REPLACE_EXISTING);
-        }
-    }
-
-    public void copyContent(File source, File target) {
-
-    }
-
-    //check files exist in target and not found in source
-    public void syncFolderDelete(File source , File target) throws IOException {
-        for (File i : target.listFiles()){
-            if (isExist(i.getName(), source.getAbsolutePath())) {
-                if (i.isDirectory()) {
-                    System.out.println(i.getAbsolutePath() + " is a Directory and Exist");
-                    syncFolderDelete(i, new File(source.getAbsolutePath() + File.separator + i.getName()));
-                }else{
-                    continue;
-                }
-            } else {
-                //delete from source
-            }
-        }
+        Files.copy(source.toPath() , new File(target.getAbsolutePath(),source.getName()).toPath(), StandardCopyOption.REPLACE_EXISTING);
     }
 
     public void delete(File file){
--- src/run/Main.java
+++ src/run/Main.java
@@ -4,15 +4,23 @@ import SyncFile.Synchronize;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
         File source = new File("J:/B");
         File target = new File("J:/A");
         Synchronize sync = new Synchronize();
-       sync.syncFolderEdit(source,target);
+        sync.syncFolder(source,target);
+
+
 	}
 }
 
